c0106449ebc5c880b90aa0f96041a65ed7d8279f
Max <max.bodifee@gmail.com>
2023-07-01 15:37:53 -0700

    Added Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("MouseButton1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vehicle currentVehicle = hit.transform.GetComponent<Vehicle>();
+
+                if (currentVehicle != null)
+                {
+                    currentVehicle.Click();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -21,16 +21,33 @@ public class Vehicle : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (moving) StartMove();
+        else EndMove();
     }
 
-    public void StartMove()
+    protected void StartMove()
     {
         
     }
 
-    public void EndMove() 
+    protected void EndMove() 
     {
         
+    }
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+    }
+
+    public void Click()
+    {
+        moving = !moving;
     }
 }
