74e50be5e721592de9fd09be5909ac71c93a2b0b
Tom Black <tomblackbusiness@gmail.com>
2023-07-01 20:54:51 -0700

    Score UI and 2D package and scale up cars

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager _instance;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
 
     private int currentScore;
     private int highScore;
@@ -13,11 +16,13 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         _instance = this;
+        scoreText.text = currentScore.ToString("N0");
     }
 
     public void AddScore(int amount)
     {
         currentScore += amount;
+        scoreText.text = currentScore.ToString("N0");
     }
 
     public void Die()
