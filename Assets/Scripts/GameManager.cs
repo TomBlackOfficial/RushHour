c2e34d4ad3023975a1ebddb6a2ea12ebb6c2239c
Tom Black <tomblackbusiness@gmail.com>
2023-07-01 18:08:58 -0700

    added score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager _instance;
+
+    private int currentScore;
+    private int highScore;
+
+    private void Awake()
     {
-        
+        _instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int amount)
     {
-        
+        currentScore += amount;
+    }
+
+    public void Die()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            if (currentScore > PlayerPrefs.GetInt("highScore"))
+            {
+                highScore = currentScore;
+                PlayerPrefs.SetInt("highScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
+        else
+        {
+            if (currentScore > highScore)
+            {
+                highScore = currentScore;
+                PlayerPrefs.SetInt("highScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
     }
 }
